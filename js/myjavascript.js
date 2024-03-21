1da743963e11466d9acd875716a9a97a288d80da
Vasco Gavino <vascogavino@gmail.com>
2024-03-21 15:37:56 +0000

    create task menu open and close

---
+++ js/myjavascript.js
@@ -0,0 +1,9 @@
+const createtaskmenu = document.querySelector(".createtask")
+
+function createtaskopen(){
+    createtaskmenu.style.display = 'block';
+}
+
+function createtaskclose(){
+    createtaskmenu.style.display = 'none';
+}
